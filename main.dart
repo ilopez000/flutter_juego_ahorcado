a5184c82a744ce966c6c933ed67d23bc07673995
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-10-24 20:02:40 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(AhorcadoApp());
+
+class AhorcadoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ahorcado Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: AhorcadoHomePage(),
+    );
+  }
+}
+
+class AhorcadoHomePage extends StatefulWidget {
+  @override
+  _AhorcadoHomePageState createState() => _AhorcadoHomePageState();
+}
+
+class _AhorcadoHomePageState extends State<AhorcadoHomePage> {
+  final List<String> palabras = ["FLUTTER", "DART", "DOSMIL", "CERTIFICADO"];
+  String palabraSeleccionada = '';
+  String palabraActual = '';
+  List<String> letrasAdivinadas = [];
+  int fallos = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    iniciarJuego();
+  }
+
+  void iniciarJuego() {
+    setState(() {
+      palabraSeleccionada = palabras[Random().nextInt(palabras.length)];
+      palabraActual = '_' * palabraSeleccionada.length;
+      letrasAdivinadas.clear();
+      fallos = 0;
+    });
+  }
+
+  void adivinarLetra(String letra) {
+    if (!palabraSeleccionada.contains(letra) && !letrasAdivinadas.contains(letra)) {
+      setState(() {
+        fallos++;
+      });
+    }
+
+    if (!letrasAdivinadas.contains(letra)) {
+      setState(() {
+        letrasAdivinadas.add(letra);
+        palabraActual = actualizarPalabraActual(palabraSeleccionada, letrasAdivinadas);
+      });
+    }
+
+    if (palabraActual == palabraSeleccionada || fallos == 6) {
+      Future.delayed(Duration(seconds: 2), () {
+        iniciarJuego();
+      });
+    }
+  }
+
+  String actualizarPalabraActual(String palabra, List<String> adivinadas) {
+    String nuevaPalabra = '';
+    for (int i = 0; i < palabra.length; i++) {
+      if (adivinadas.contains(palabra[i])) {
+        nuevaPalabra += palabra[i];
+      } else {
+        nuevaPalabra += '_';
+      }
+    }
+    return nuevaPalabra;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ahorcado en Flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Fallos: $fallos / 6'),
+            Text(palabraActual, style: TextStyle(fontSize: 24)),
+            Wrap(
+              children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((e) {
+                return Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ElevatedButton(
+                    child: Text(e),
+                    onPressed: letrasAdivinadas.contains(e) ? null : () => adivinarLetra(e),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
